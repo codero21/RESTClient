5932cf5135327b43ee98a92025a00ce4174fae9b
Rollin Francois <Rollin@Rollins-MacBook-Pro.local>
2017-08-22 22:48:27 -0400

    Add a tap gesture recognizer and use it to dismiss the keyboard when the background area of the view is tapped. - turned off...

--- RESTClient/ViewController.swift
+++ RESTClient/ViewController.swift
@@ -20,17 +20,67 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     @IBAction func onDismissKeyboard(_ sender: Any) {
         radiusField.resignFirstResponder()
+        
     }
     
     @IBAction func onCalculateArea(_ sender: Any) {
+        
+        let radius: String = radiusField.text!
+        
+        if radius.isEmpty {
+            return
+        }
+        
+        let serviceURL: String = "http://www.asmtechnology.com/MathService/CircleArea/?radius=\(radius)"
+        
+        let url: URL! = URL(string: serviceURL)
+        
+        let request: URLRequest = URLRequest(url: url)
+        
+        let configuration = URLSessionConfiguration.default
+        configuration.timeoutIntervalForRequest = 15.0
+        
+        let session: URLSession! = URLSession(configuration: configuration, delegate: nil, delegateQueue: OperationQueue.main)
+        
+        
+        let task = session.dataTask(with: request, completionHandler: { (data, respose, error) in
+                                if data != nil {
+                            let decodedString = String(data: data!,
+                                                       encoding: String.Encoding.utf8)
+                            self.serverResponseView.text = decodedString!
+                        }
+                        
+                    });
+        
+        task.resume()
+        
     }
+    
+    
+    
+    // MARK: Methods
+    
+    func handleBackgroundTap(sender: UITapGestureRecognizer) {
+        radiusField.resignFirstResponder()
+    }
+    
+    
     
 
     // MARK: Boiler Plate 
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // let tapRecognizer = UITapGestureRecognizer(target: self, action: Selector(("handleBackgroundTap:")))
+        
+        // tapRecognizer.cancelsTouchesInView = false
+        
+        // self.view.addGestureRecognizer(tapRecognizer)
+        
+        // clear the initial content of the text view when the ciew is loaded.
+        serverResponseView.text = ""
+        
     }
 
     override func didReceiveMemoryWarning() {
